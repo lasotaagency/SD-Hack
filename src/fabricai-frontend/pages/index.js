127a87431f1203d57c6ad5cee15a46791e0a3914
Harsha Bommana <harshabommana@Harshas-MacBook-Pro.local>
2023-04-21 05:54:03 +0530

    Frontend works

--- src/fabricai-frontend/pages/index.js
+++ src/fabricai-frontend/pages/index.js
@@ -2,78 +2,26 @@ import Dropdown from "react-dropdown";
 import Image from "next/image";
 import { useState } from "react";
 
-const SideBar = ({ articles }) => {
-  const [inputOption, setInputOption] = useState('text');
-
-  return (
-    <div className="w-[20%] bg-[#088395] flex flex-col content-center text-center relative">
-      {articles.length > 0 && (
-        <>
-          {/* <h3 className="text-[#0A4D68]">Autodetected Clothing Options</h3> */}
-          <div className="mt-32">Autodetected Clothing Options</div>
-          <Dropdown
-            options={articles}
-            className="w-[80%] mt-4 bg-[#D9D9D9] p-2 text-[#0A4D68] mx-auto"
-          />
-          <div className="mt-32">
-            <div>
-              <label>
-                <input
-                  type="radio"
-                  value="text"
-                  checked={inputOption === 'text'}
-                  onChange={(e) => setInputOption(e.target.value)}
-                />
-                Text Based Edit
-              </label>
-            </div>
-            <div>
-              <label>
-                <input
-                  type="radio"
-                  value="image"
-                  checked={inputOption === 'image'}
-                  onChange={(e) => setInputOption(e.target.value)}
-                />
-                Image Based Edit
-              </label>
-            </div>
-          </div>
-          {inputOption === 'text' && (
-            <>
-              <input className="w-[80%] bg-[#D9D9D9] p-2 mt-2 text-[#0A4D68] mx-auto" />
-            </>
-          )}
-          {inputOption === 'image' && (
-            <>
-              <input
-                type="file"
-                className="w-[80%] bg-[#D9D9D9] p-2 mt-2 text-[#0A4D68] mx-auto"
-              />
-            </>
-          )}
-          <button className="w-[80%] bg-[#D9D9D9] p-2 mt-2 text-[#0A4D68] mx-auto">
-            Submit
-          </button>
-        </>
-      )}
-      <Image
-        src={"/fabric.ai.png"}
-        width={120}
-        height={200}
-        className="absolute bottom-8 left-0 right-0 ml-auto mr-auto"
-      />
-    </div>
-  );
-};
-
 export default function Home() {
   const [selectedFile, setSelectedFile] = useState(null);
-  const [image, setImage] = useState(null);
+  const [additionalFile, setAdditionalFile] = useState(null);
+  const [currImageURL, setCurrImageURL] = useState(null);
+  const [prevImageURLs, setPrevImageURLs] = useState([]);
   const [articles, setArticles] = useState([]);
+  const [selectedArticle, setSelectedArticle] = useState("");
+
+  const [inputOption, setInputOption] = useState("");
+  const [textInput, setTextInput] = useState("");
+  const [imageInput, setImageInput] = useState("");
+
+  const baseURI = "https://f034-108-234-21-0.ngrok-free.app";
 
   const handleFileSelect = (event) => {
     setSelectedFile(event.target.files[0]);
+  };
+
+  const handleAdditionalFileSelect = (event) => {
+    setAdditionalFile(event.target.files[0]);
   };
 
   const handleUpload = async () => {
@@ -83,54 +31,226 @@ export default function Home() {
     formData.append("file", selectedFile);
 
     try {
-      const response = await fetch(
-        "https://8005-108-234-21-0.ngrok-free.app/upload_image",
-        {
-          method: "POST",
-          body: formData,
-        }
-      )
+      const response = await fetch(`${baseURI}/upload_image`, {
+        method: "POST",
+        body: formData,
+      })
         .then((res) => res.json())
         .then((data) => {
           console.log(data);
           setArticles(data["articles_detected"]);
-          setImage(selectedFile);
+          setCurrImageURL(baseURI + data["image_url"]);
+          // setPrevImageURLs((prevImageURLs) => [
+          //   ...prevImageURLs,
+          //   baseURI + data["image_url"],
+          // ]);
         });
     } catch (error) {
       console.error(error);
     }
   };
 
+  const handleSubmit = async () => {
+    console.log("submit");
+
+    if (inputOption === "text") {
+      console.log(textInput);
+      const response = await fetch(
+        "https://f034-108-234-21-0.ngrok-free.app/submit",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            image_url: currImageURL,
+            condition_data: textInput,
+            condition_type: "text",
+            option: selectedArticle,
+          }),
+        }
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          const generatedUrl = baseURI + data["generated_images"][0].slice(1);
+          const generatedUrl2 = baseURI + data["generated_images"][1].slice(1);
+          setPrevImageURLs((prevImageURLs) => [
+            ...prevImageURLs,
+            generatedUrl,
+            generatedUrl2,
+          ]);
+          setCurrImageURL(generatedUrl2);
+        });
+    } else if (inputOption === "image") {
+      const additionalFormData = new FormData();
+      additionalFormData.append("additional_image", additionalFile);
+      console.log(additionalFile);
+
+      const response = await fetch(
+        "https://f034-108-234-21-0.ngrok-free.app/upload_additional_image",
+        {
+          method: "POST",
+          body: additionalFormData,
+        }
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          console.log("HERE");
+          const response = fetch(
+            "https://f034-108-234-21-0.ngrok-free.app/submit",
+            {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({
+                image_url: currImageURL,
+                condition_data:
+                  "https://f034-108-234-21-0.ngrok-free.app" +
+                  data["image_url"],
+                condition_type: "image",
+                option: selectedArticle,
+              }),
+            }
+          )
+            .then((res) => res.json())
+            .then((data) => {
+              console.log(data);
+              const generatedUrl =
+                baseURI + data["generated_images"][0].slice(1);
+              const generatedUrl2 =
+                baseURI + data["generated_images"][1].slice(1);
+              setPrevImageURLs((prevImageURLs) => [
+                ...prevImageURLs,
+                generatedUrl,
+                generatedUrl2,
+              ]);
+              setCurrImageURL(generatedUrl2);
+            });
+        });
+    }
+  };
+
+  const handlePreviousImageClick = (index) => {
+    // Set the current image URL to the URL of the selected previous image
+    const tempCurrImageURL = currImageURL;
+    setCurrImageURL(prevImageURLs[index]);
+    setPrevImageURLs((prevImageURLs) => [
+      ...prevImageURLs.slice(0, index),
+      tempCurrImageURL,
+      ...prevImageURLs.slice(index + 1),
+    ]);
+  };
+
   return (
     <>
       <div className="h-screen w-screen flex flex-row justify-stretch content-stretch">
-        <SideBar articles={articles} />
-        <div className="w-full h-full bg-[#FEEAC2] flex flex-col justify-center content-center text-black">
-          <div className="flex flex-col bg-[#d8a640] w-fit mx-auto rounded-md p-4">
-            {image && (
-              <img
-                src={URL.createObjectURL(image)}
-                alt="uploaded"
-                className="w-64 h-64"
+        <div className="w-[20%] bg-[#088395] flex flex-col content-center text-center relative">
+          {articles.length > 0 && (
+            <>
+              {/* <h3 className="text-[#0A4D68]">Autodetected Clothing Options</h3> */}
+              <div className="mt-32">Autodetected Clothing Options</div>
+              <Dropdown
+                options={articles}
+                className="w-[80%] mt-4 bg-[#D9D9D9] p-2 text-[#0A4D68] mx-auto"
+                onChange={(e) => {
+                  setSelectedArticle(e.value);
+                }}
               />
-            )}
+              <div className="mt-32">
+                <div>
+                  <label>
+                    <input
+                      type="radio"
+                      value="text"
+                      checked={inputOption === "text"}
+                      onChange={(e) => setInputOption(e.target.value)}
+                    />
+                    Text Based Edit
+                  </label>
+                </div>
+                <div>
+                  <label>
+                    <input
+                      type="radio"
+                      value="image"
+                      checked={inputOption === "image"}
+                      onChange={(e) => setInputOption(e.target.value)}
+                    />
+                    Image Based Edit
+                  </label>
+                </div>
+              </div>
+              {inputOption === "text" && (
+                <>
+                  <input
+                    className="w-[80%] bg-[#D9D9D9] p-2 mt-2 text-[#0A4D68] mx-auto"
+                    onChange={(e) => setTextInput(e.target.value)}
+                  />
+                </>
+              )}
+              {inputOption === "image" && (
+                <>
+                  <input
+                    type="file"
+                    className="w-[80%] bg-[#D9D9D9] p-2 mt-2 text-[#0A4D68] mx-auto"
+                    onChange={handleAdditionalFileSelect}
+                  />
+                </>
+              )}
+              <button
+                className="w-[80%] bg-[#D9D9D9] p-2 mt-2 text-[#0A4D68] mx-auto"
+                onClick={handleSubmit}
+              >
+                Submit
+              </button>
+            </>
+          )}
+          <Image
+            src={"/fabric.ai.png"}
+            width={120}
+            height={200}
+            className="absolute bottom-8 left-0 right-0 ml-auto mr-auto"
+          />
+        </div>
+        <div className="w-full h-full bg-[#FEEAC2] flex flex-col justify-center content-center text-black">
+          {currImageURL && (
+            <img
+              src={currImageURL}
+              alt="uploaded"
+              className="h-[256px] w-auto mx-auto"
+            />
+          )}
 
-            {!image && (
-              <>
-                <input
-                  type="file"
-                  className="p-2"
-                  onChange={handleFileSelect}
-                />
-                <button
-                  className="p-2 w-fit mx-auto bg-gray-600 text-white"
-                  onClick={handleUpload}
-                >
-                  Upload
-                </button>
-              </>
-            )}
-          </div>
+          {!currImageURL && (
+            <>
+              <input type="file" className="p-2" onChange={handleFileSelect} />
+              <button
+                className="p-2 w-fit mx-auto bg-gray-600 text-white"
+                onClick={handleUpload}
+              >
+                Upload
+              </button>
+            </>
+          )}
+
+          {prevImageURLs.length > 0 && (
+            <>
+              <h3 className="text-center mt-16">Previous Images</h3>
+              <div className="flex flex-wrap justify-center gap-x-4 flex-row-reverse">
+                {prevImageURLs.map((url, index) => (
+                  <img
+                    src={url}
+                    alt="uploaded"
+                    className="h-[256px] w-auto"
+                    onClick={() => handlePreviousImageClick(index)}
+                    key={index}
+                  />
+                ))}
+              </div>
+            </>
+          )}
         </div>
       </div>
     </>
